2d6e8fd569ec73e59309d68de99e3b434654265b
skdziwak <skdziwak@gmail.com>
2021-04-26 10:34:30 +0200

    Admin panel and menu

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    httpGetAsync('menu.txt', (txt) => {
+        console.log(txt)
+    });
+});
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
